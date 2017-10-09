7e5a55a3adb42c7df90944c100a46e300b6d6291
Michael Mammoliti <mammoliti.michael@gmail.com>
2017-10-09 16:07:30 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+const generateReducer = options => {
+  var { initialState, constants, actionPrefix } = options;
+
+  return (state = initialState, action) => {
+    if (!action) return state;
+
+    const { type, payload } = action;
+
+    const actionIdentifier = type.split('/');
+    const actionIdentifierName = actionIdentifier[1];
+
+    const actionName = (actionIdentifier.length)
+      ? actionIdentifierName
+      : type.replace(`${actionPrefix}/`, '')
+    ;
+
+    if (constants[actionName]) return { ...state, ...payload };
+
+    return state;
+  };
+};
